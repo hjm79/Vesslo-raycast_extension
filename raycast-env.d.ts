3c36da9f26e878aa8b71404cc7da5f5a73b5e447
hjm79.top <me@hjm79.top>
2026-01-16 18:04:01 +0900

    Initial commit: Vesslo Raycast Extension with Search, Updates, and Tag Browsing

---
+++ raycast-env.d.ts
@@ -0,0 +1,36 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `search-apps` command */
+  export type SearchApps = ExtensionPreferences & {}
+  /** Preferences accessible in the `updates` command */
+  export type Updates = ExtensionPreferences & {}
+  /** Preferences accessible in the `bulk-homebrew-update` command */
+  export type BulkHomebrewUpdate = ExtensionPreferences & {}
+  /** Preferences accessible in the `browse-by-tag` command */
+  export type BrowseByTag = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `search-apps` command */
+  export type SearchApps = {}
+  /** Arguments passed to the `updates` command */
+  export type Updates = {}
+  /** Arguments passed to the `bulk-homebrew-update` command */
+  export type BulkHomebrewUpdate = {}
+  /** Arguments passed to the `browse-by-tag` command */
+  export type BrowseByTag = {}
+}
+
